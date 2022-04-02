289bc29e5c3e3ecc39f38bba94dd3eec7857c6c8
Ulysses Bonfim <ulysses@inplace.be>
2022-04-02 05:08:51 +0200

    Remove wait

--- src/booking/main.js
+++ src/booking/main.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-var */
+import Navigation from './navigation'
 import './style.css'
 
 /**
@@ -6,10 +7,8 @@ import './style.css'
  */
 const sliderController = () => {
     //  ONly starts after page is loaded
-    // setTimeout(() => {
-    //     const navController = new Navigation()
-    //     navController.init()
-    // }, 300)
+    const navController = new Navigation()
+    navController.init()
 }
 
 // eslint-disable-next-line no-use-before-define

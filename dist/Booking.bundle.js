86469470b334281231997761ee2cbb228ba632ce
Ulysses Bonfim <ulysses@inplace.be>
2022-04-04 05:02:39 +0200

    Form submit

--- dist/Booking.bundle.js
+++ dist/Booking.bundle.js
@@ -11872,13 +11872,38 @@ __webpack_require__.r(__webpack_exports__);
 
 
 /**
+ * Form Submission
+ * @param {SubmitEvent} event
+ */
+
+const onSubmit = async event => {
+  event.preventDefault();
+  const form = event.target;
+  const data = new FormData(form);
+  const json = Object.fromEntries(data.entries());
+  const url = new URL(form.attr('action'));
+  const res = await fetch(url, {
+    method: 'POST',
+    headers: {
+      contentType: 'application/json',
+      dataType: 'json'
+    },
+    body: json
+  });
+  const resp = await res.json();
+  console.log(JSON.stringify(resp));
+};
+/**
  * Add handlers
  */
+
 
 const sliderController = () => {
   //  ONly starts after page is loaded
   const navController = new _navigation__WEBPACK_IMPORTED_MODULE_0__["default"]();
-  navController.init();
+  navController.init(); // Setup form submit action
+
+  document.getElementById('booking-form').onsubmit = onSubmit;
 }; // eslint-disable-next-line no-use-before-define
 
 
--- src/booking/main.js
+++ src/booking/main.js
@@ -3,12 +3,41 @@ import Navigation from './navigation'
 import './style.css'
 
 /**
+ * Form Submission
+ * @param {SubmitEvent} event
+ */
+const onSubmit = async event => {
+    event.preventDefault()
+    const form = event.target
+    const data = new FormData(form)
+    const json = Object.fromEntries(data.entries())
+
+    const url = new URL(form.attr('action'))
+
+    const res = await fetch(url, {
+        method: 'POST',
+        headers: {
+            contentType: 'application/json',
+            dataType: 'json'
+        },
+        body: json
+    })
+
+    const resp = await res.json()
+
+    console.log(JSON.stringify(resp))
+}
+
+/**
  * Add handlers
  */
 const sliderController = () => {
     //  ONly starts after page is loaded
     const navController = new Navigation()
     navController.init()
+
+    // Setup form submit action
+    document.getElementById('booking-form').onsubmit = onSubmit
 }
 
 // eslint-disable-next-line no-use-before-define

2669f84de76bd995755098746cdeef9191eeb126
Ulysses Bonfim <ulysses@dover.pro>
2022-04-24 07:07:17 +0200

    Added: Estimation

--- dist/StepDuration.js
+++ dist/StepDuration.js
@@ -590,6 +590,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _controllers_sequence__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../controllers/sequence */ "./src/booking-flow/controllers/sequence.js");
 /* harmony import */ var _controllers_step__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../controllers/step */ "./src/booking-flow/controllers/step.js");
 /* harmony import */ var _helpers_dom__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../helpers/dom */ "./src/booking-flow/helpers/dom.js");
+/* eslint-disable max-classes-per-file */
+
 /* eslint-disable class-methods-use-this */
 
 /* eslint-disable vars-on-top */
@@ -600,6 +602,123 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
+/**
+ * @typedef {Object} CookieOptions
+ * @property {string} cleaning-bathrooms
+ * @property {string} cleaning-bedrooms
+ * @property {boolean} extra-cabinets
+ * @property {boolean} extra-fridge
+ * @property {boolean} extra-oven
+ * @property {boolean} extra-windows
+ * @property {string} ironing
+ * @property {string} postal-code
+ * @property {boolean} service-cleaning
+ * @property {boolean} service-cooking
+ * @property {boolean} service-grocery
+ * @property {boolean} service-ironing
+ */
+
+class OptionsDuration {
+  /**
+   * @type {CookieOptions}
+   */
+  ops;
+  /**
+   *
+   * @param {CookieOptions} options
+   */
+
+  constructor(options) {
+    this.ops = options;
+  }
+
+  get total() {
+    return this.services + this.ironing + this.cleaning;
+  }
+
+  get services() {
+    const services = Object.keys(this.ops).filter(o => o.startsWith('service-'));
+    let total = 0;
+
+    if (services.length > 1) {
+      total += this.ops['service-cooking'] ? 0.5 : 0;
+      total += this.ops['service-grocery'] ? 0.5 : 0;
+    }
+
+    return total;
+  }
+
+  get ironing() {
+    if (!this.ops['service-ironing']) return 0;
+    const ironing = this.ops.ironing.replace('ironing-size-', '');
+
+    switch (ironing) {
+      case 'xs':
+        return 0.5;
+
+      case 's':
+        return 1;
+
+      case 'm':
+        return 2;
+
+      case 'l':
+        return 3;
+
+      case 'xl':
+        return 4;
+
+      default:
+        return 0;
+    }
+  }
+
+  get cleaning() {
+    if (!this.ops['service-cleaning']) return 0; // Extras
+
+    let total = this.ops['extra-windows'] ? 1 : 0;
+    total += this.ops['extra-cabinets'] ? 1 : 0;
+    total += this.ops['extra-fridge'] ? 0.5 : 0;
+    total += this.ops['extra-oven'] ? 0.5 : 0; // Bedroom
+
+    const bedroom = this.ops['cleaning-bedrooms'];
+    const bathroom = this.ops['cleaning-bathrooms'];
+
+    switch (bedroom) {
+      case '3':
+      case '4':
+        total += 1;
+        break;
+
+      case '5+':
+        total += 2;
+        break;
+
+      default:
+        break;
+    }
+
+    switch (bathroom) {
+      case '2':
+        total += 1;
+        break;
+
+      case '3':
+        total += 2;
+        break;
+
+      case '4+':
+        total += 3;
+        break;
+
+      default:
+        break;
+    }
+
+    return total;
+  }
+
+}
 
 class Step extends _controllers_step__WEBPACK_IMPORTED_MODULE_1__["default"] {
   cookie;
@@ -607,6 +726,27 @@ class Step extends _controllers_step__WEBPACK_IMPORTED_MODULE_1__["default"] {
   constructor() {
     super(_controllers_sequence__WEBPACK_IMPORTED_MODULE_0__.STEP.Duration);
     this.cookie = window.FpCookie;
+  }
+  /**
+   * Calculate estimation
+   */
+
+
+  init() {
+    super.init();
+    this.estimation = this.estimation;
+  }
+
+  get estimation() {
+    const options = new OptionsDuration(this.cookie.store);
+    const {
+      total
+    } = options;
+    return total;
+  }
+
+  set estimation(total) {
+    _helpers_dom__WEBPACK_IMPORTED_MODULE_2__["default"].id('duration').nextElementSibling.noUiSlider.set(total);
   }
   /**
    * @returns {boolean}

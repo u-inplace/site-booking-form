17916c42a607b681b3a4ceb4b9ee193fdb4fef63
Ulysses Bonfim <ulysses@inplace.be>
2022-08-27 19:52:55 +0200

    Changed: step initialization routine. Added check DOMContentLoaded

--- src/booking/packages/ironing.js
+++ src/booking/packages/ironing.js
@@ -20,8 +20,14 @@ class Step extends StepController {
     }
 }
 
-// Wait for DOM to load before query elements
-document.addEventListener('DOMContentLoaded', () => {
+const run = () => {
+    console.log('Step: DOMContentLoaded')
     const step = new Step()
     step.init()
-})
+}
+
+// Wait for DOM to load before query elements
+// It's possible that DOMContent is already loaded, so check on document.readState
+console.log('Step: Script loaded')
+if (document.readyState !== 'loading') run()
+else document.addEventListener('DOMContentLoaded', run)

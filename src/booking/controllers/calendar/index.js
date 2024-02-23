8a54b54992e738f5cef4982e7672d6a5355b905e
Ulysses Bonfim <ulyssesb@gmail.com>
2024-02-23 13:55:55 +0100

    💩 ➕ log

--- src/booking/controllers/calendar/index.js
+++ src/booking/controllers/calendar/index.js
@@ -241,7 +241,9 @@ export default class CalendarController {
         console.log(`getAvailability | currentWeekKey: ${currentWeekKey}`)
         console.log(`getAvailability | this.#cached: ${JSON.stringify(this.#cached)}`)
         console.log(`getAvailability | this.#cached[weekKey]: ${this.#cached[weekKey]}`)
-        console.log(`getAvailability | run: ${weekKey < currentWeekKey || this.#cached[weekKey]}`)
+        console.log(
+            `getAvailability | return: ${weekKey < currentWeekKey || this.#cached[weekKey]}`
+        )
 
         // Compare week number instead of date to avoid not requesting availability
         // for the current week if the weekStartDate is in the past
